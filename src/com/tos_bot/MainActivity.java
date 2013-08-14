30be4bdbfee6829a1c0352659b7e0e83841f1d7e
FrankWang <eternnoir@gmail.com>
2013-08-14 15:45:17 +0000

    add listener to stop & start

--- src/com/tos_bot/MainActivity.java
+++ src/com/tos_bot/MainActivity.java
@@ -4,15 +4,36 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	private Button _startServiceButton;
+	private Button _stopServiceButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Intent intent = new Intent(MainActivity.this, botService.class);
-		startService(intent);
+		_startServiceButton = (Button) findViewById(R.id.start_button);
+		_stopServiceButton = (Button) findViewById(R.id.stop_button);
+
+		_startServiceButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				Intent intent = new Intent(MainActivity.this, botService.class);
+				startService(intent);
+			}
+		});
+		
+		_stopServiceButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, botService.class);
+				stopService(intent);
+				
+			}
+		});
 	}
 
 	@Override
