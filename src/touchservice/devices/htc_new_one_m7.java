5a42a360efa364334b9032a4bc5b285a0b2dad5d
FrankWang <eternnoir@gmail.com>
2013-08-19 21:54:51 +0000

    improve htc one m7 performace

--- src/touchservice/devices/htc_new_one_m7.java
+++ src/touchservice/devices/htc_new_one_m7.java
@@ -7,74 +7,63 @@ import puzzleslove.solution;
 import touchservice.AbstractTouchService;
 import touchservice.touchpos;
 
-public class htc_new_one_m7 extends AbstractTouchService{
-	
+public class htc_new_one_m7 extends AbstractTouchService {
+
 	@Override
-	public  Vector<String> touchDown(int x,int y){
+	public Vector<String> touchDown(int x, int y) {
 		Vector<String> cl = new Vector<String>();
-		cl.add("sendevent /dev/input/event3 3 53 "+x+"\n");
-		cl.add("sendevent /dev/input/event3 3 54 "+y+"\n");
+		cl.add("sendevent /dev/input/event3 3 53 " + x + "\n");
+		cl.add("sendevent /dev/input/event3 3 54 " + y + "\n");
 		cl.add("sendevent /dev/input/event3 3 57 105\n");
 		cl.add("sendevent /dev/input/event3 0 0 0\n");
 		return cl;
 	}
+
 	@Override
-	public  Vector<String> touchUp(){
+	public Vector<String> touchUp() {
 		Vector<String> cl = new Vector<String>();
 		cl.add("sendevent /dev/input/event3 3 57 4294967295\n");
 		cl.add("sendevent /dev/input/event3 0 0 0 \n");
 		return cl;
 	}
+
 	@Override
-	public  Vector<String> touchMove(int x1, int y1, int x2, int y2, int gap){
+	public Vector<String> touchMove(int x1, int y1, int x2, int y2, int gap) {
 		Vector<String> cl = new Vector<String>();
-		int x=x1;
-		int y=y1;
-		int xGap = (x2-x1)/gap;
-		int yGap = (y2-y1)/gap;
-		for(int g=0;g<gap;g++){
-			x=x1+g*xGap;
-			y=y1+g*yGap;
+		int x = x1;
+		int y = y1;
+		int xGap = (x2 - x1) / gap;
+		int yGap = (y2 - y1) / gap;
+		for (int g = 0; g < gap; g++) {
+			x = x1 + g * xGap;
+			y = y1 + g * yGap;
 			// send x command
-			cl.add("sendevent /dev/input/event3 3 53 "+ x +"\n");
+			cl.add("sendevent /dev/input/event3 3 53 " + x + "\n");
 			cl.add("sendevent /dev/input/event3 0 0 0 \n");
-			//send y command
-			cl.add("sendevent /dev/input/event3 3 54 "+ y +"\n");
+			// send y command
+			cl.add("sendevent /dev/input/event3 3 54 " + y + "\n");
 			cl.add("sendevent /dev/input/event3 0 0 0 \n");
-			
+
 		}
 		return cl;
 	}
+
 	@Override
-	public  Vector<String> touchMoveX(int x1,  int x2,  int gap){
+	public Vector<String> touchMoveX(int x1, int x2, int gap) {
 		Vector<String> cl = new Vector<String>();
-		int x=x1;
+		cl.add("sendevent /dev/input/event3 3 53 " + x2 + "\n");
+		cl.add("sendevent /dev/input/event3 0 0 0 \n");
 
-		int xGap = (x2-x1)/gap;
-
-		for(int g=0;g<gap;g++){
-			x=x1+g*xGap;
-			// send x command
-			cl.add("sendevent /dev/input/event3 3 53 "+ x +"\n");
-			cl.add("sendevent /dev/input/event3 0 0 0 \n");
-			
-		}
 		return cl;
 	}
+
 	@Override
-	public  Vector<String> touchMoveY(int y1,  int y2, int gap){
+	public Vector<String> touchMoveY(int y1, int y2, int gap) {
 		Vector<String> cl = new Vector<String>();
-		int y=y1;
-		int yGap = (y2-y1)/gap;
-		for(int g=0;g<gap;g++){
-			y=y1+g*yGap;
-			//send y command
-			cl.add("sendevent /dev/input/event3 3 54 "+ y +"\n");
-			cl.add("sendevent /dev/input/event3 0 0 0 \n");
-			
-		}
+		cl.add("sendevent /dev/input/event3 3 54 " + y2 + "\n");
+		cl.add("sendevent /dev/input/event3 0 0 0 \n");
+
 		return cl;
 	}
-
 
 }
