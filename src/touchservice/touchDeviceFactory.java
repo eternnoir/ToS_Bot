211ba543705d469ddfcce59d753878cbc0dfb51f
FrankWang <eternnoir@gmail.com>
2013-08-19 13:59:34 +0000

    add inix,y for htc one

--- src/touchservice/touchDeviceFactory.java
+++ src/touchservice/touchDeviceFactory.java
@@ -1,6 +1,7 @@
 package touchservice;
 
 import touchservice.devices.htc_new_one_m7;
+import touchservice.devices.htc_one;
 
 public class touchDeviceFactory {
 	public static String[] getDeviceList(){
@@ -16,6 +17,8 @@ public class touchDeviceFactory {
 			ret.setUp(270, 135, 1380);
 			return ret;
 		}else 	if(deviceName.equals("htc_one")){
+			AbstractTouchService ret = new htc_one();
+			ret.setUp(180, 90, 890);
 			return new htc_new_one_m7();
 		}
 		
