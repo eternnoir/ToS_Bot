4c358c1c38c6c67ccf07f61a451eb17431c3e75a
tp6vup54 <genius091612@gmail.com>
2013-08-19 17:21:38 +0800

    fix device selection

--- src/com/tos_bot/MainActivity.java
+++ src/com/tos_bot/MainActivity.java
@@ -12,66 +12,67 @@ import android.widget.EditText;
 import android.widget.Spinner;
 
 public class MainActivity extends Activity {
-	private Button _startServiceButton;
-	private Button _stopServiceButton;
+    private Button _startServiceButton;
+    private Button _stopServiceButton;
+    private Spinner _deviceS;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		this.initDeviceList();		//create device list
-		_startServiceButton = (Button) findViewById(R.id.start_button);
-		_stopServiceButton = (Button) findViewById(R.id.stop_button);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.initDeviceList();		//create device list
+        _startServiceButton = (Button) findViewById(R.id.start_button);
+        _stopServiceButton = (Button) findViewById(R.id.stop_button);
+        _deviceS = (Spinner)findViewById(R.id.deviceList);
 
-		_startServiceButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View view) {
-				EditText serveret;
-				EditText deepet;
-				serveret = (EditText) findViewById(R.id.serverUrlText);
-				deepet = (EditText) findViewById(R.id.maxMoveText);
-				ConfigData.Serverurl = serveret.getText().toString();
-				ConfigData.deep = Integer.parseInt(deepet.getText().toString());
-				Intent intent = new Intent(MainActivity.this, botService.class);
-				startService(intent);
+        _startServiceButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                EditText serveret;
+                EditText deepet;
+                serveret = (EditText) findViewById(R.id.serverUrlText);
+                deepet = (EditText) findViewById(R.id.maxMoveText);
+                ConfigData.DeviceName = _deviceS.getSelectedItem().toString();
+                ConfigData.Serverurl = serveret.getText().toString();
+                ConfigData.deep = Integer.parseInt(deepet.getText().toString());
+                Intent intent = new Intent(MainActivity.this, botService.class);
+                startService(intent);
 
-			}
-		});
+            }
+        });
 
-		_stopServiceButton.setOnClickListener(new View.OnClickListener() {
+        _stopServiceButton.setOnClickListener(new View.OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				ConfigData.pasCDid = "";
-				Intent intent = new Intent(MainActivity.this, botService.class);
-				stopService(intent);
-			}
-		});
+            @Override
+            public void onClick(View v) {
+                ConfigData.pasCDid = "";
+                Intent intent = new Intent(MainActivity.this, botService.class);
+                stopService(intent);
+            }
+        });
 
-	}
+    }
 
-	@Override
-	public void onStart() {
-		super.onStart();
-		ConfigData.pasCDid = "";
-		Spinner deviceS = (Spinner)findViewById(R.id.deviceList);
-		ConfigData.DeviceName = deviceS.getSelectedItem().toString();
-		Intent intent = new Intent(MainActivity.this, botService.class);
-		stopService(intent);
-	}
+    @Override
+    public void onStart() {
+        super.onStart();
+        ConfigData.pasCDid = "";
+        Intent intent = new Intent(MainActivity.this, botService.class);
+        stopService(intent);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-	private void initDeviceList() {
-		Spinner spinner = (Spinner) findViewById(R.id.deviceList);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_spinner_item,touchDeviceFactory.getDeviceList());
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner.setAdapter(adapter);
-	}
+    private void initDeviceList() {
+        Spinner spinner = (Spinner) findViewById(R.id.deviceList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,touchDeviceFactory.getDeviceList());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
 
 }
