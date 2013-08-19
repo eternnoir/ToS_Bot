5afab2438bcd7fa848d722dd5311116b0b293676
FrankWang <eternnoir@gmail.com>
2013-08-19 20:59:59 +0800

    support genymotion Nexus One 540x960 240dpi

--- src/touchservice/touchDeviceFactory.java
+++ src/touchservice/touchDeviceFactory.java
@@ -1,24 +1,25 @@
 package touchservice;
 
+import touchservice.devices.Gen_nexus_one;
 import touchservice.devices.htc_new_one_m7;
 import touchservice.devices.htc_one_x;
 
 public class touchDeviceFactory {
-	public static String[] getDeviceList(){
-		return new String[]{
-				"htc_new_one_m7",
-				"htc_one_x"
-			};
-		}
-	
-	public static AbstractTouchService getNewTouchService(String deviceName){
-        AbstractTouchService ret = null;
-		if(deviceName.equals("htc_new_one_m7")){
+	public static String[] getDeviceList() {
+		return new String[] { "htc_new_one_m7", "htc_one_x", "Gen_nexus_one" };
+	}
+
+	public static AbstractTouchService getNewTouchService(String deviceName) {
+		AbstractTouchService ret = null;
+		if (deviceName.equals("htc_new_one_m7")) {
 			ret = new htc_new_one_m7();
 			ret.setUp(270, 135, 1380);
-		}else 	if(deviceName.equals("htc_one_x")){
+		} else if (deviceName.equals("htc_one_x")) {
 			ret = new htc_one_x();
 			ret.setUp(180, 90, 890);
+		} else if (deviceName.equals("Gen_nexus_one")) {
+			ret = new Gen_nexus_one();
+			ret.setUp(90, 45, 475);
 		}
 		return ret;
 	}
