a9dc8333036a6ef9899623ff92a4e83ef084e49f
FrankWang <eternnoir@gmail.com>
2013-08-27 00:39:41 +0000

    add others device maybe can support any device

---
+++ src/touchservice/devices/AtecDevice.java
@@ -0,0 +1,71 @@
+package touchservice.devices;
+
+import java.util.Vector;
+
+import net.atec.sender.DeviceEvent;
+
+import touchservice.AbstractTouchService;
+
+public class AtecDevice extends AbstractTouchService {
+	DeviceEvent _de;
+	public AtecDevice(DeviceEvent de){
+		_de = de;
+	}
+	@Override
+		public Vector<String> touchDown(int x, int y) {
+			Vector<String> cl = new Vector<String>();
+			cl.add("sendevent "+_de.getPosXEvent() + x + "\n");
+			cl.add("sendevent "+_de.getPosYEvent() + y + "\n");
+			cl.add("sendevent "+_de.getPresureEvent()+ "105\n");
+			cl.add("sendevent "+_de.getBlankEvent()+"\n");
+			return cl;
+		}
+	
+		@Override
+		public Vector<String> touchUp() {
+			Vector<String> cl = new Vector<String>();
+			cl.add("sendevent "+_de.getPresureEvent()+"4294967295\n");
+			cl.add("sendevent "+_de.getBlankEvent()+"\n");
+			return cl;
+		}
+	
+		@Override
+		public Vector<String> touchMove(int x1, int y1, int x2, int y2, int gap) {
+			Vector<String> cl = new Vector<String>();
+			int x = x1;
+			int y = y1;
+			int xGap = (x2 - x1) / gap;
+			int yGap = (y2 - y1) / gap;
+			for (int g = 0; g < gap; g++) {
+				x = x1 + g * xGap;
+				y = y1 + g * yGap;
+				// send x command
+				cl.add("sendevent "+_de.getPosXEvent() + x + "\n");
+				cl.add("sendevent "+_de.getBlankEvent()+"\n");
+				// send y command
+				cl.add("sendevent "+_de.getPosYEvent() + y + "\n");
+				cl.add("sendevent "+_de.getBlankEvent()+"\n");
+	
+			}
+			return cl;
+		}
+	
+		@Override
+		public Vector<String> touchMoveX(int x1, int x2, int gap) {
+			Vector<String> cl = new Vector<String>();
+			cl.add("sendevent "+_de.getPosXEvent() + x2 + "\n");
+			cl.add("sendevent "+_de.getBlankEvent()+"\n");
+	
+			return cl;
+		}
+	
+		@Override
+		public Vector<String> touchMoveY(int y1, int y2, int gap) {
+			Vector<String> cl = new Vector<String>();
+			cl.add("sendevent "+_de.getPosYEvent() + y2 + "\n");
+			cl.add("sendevent "+_de.getBlankEvent()+"\n");
+			return cl;
+		}
+	
+
+}
--- src/touchservice/touchDeviceFactory.java
+++ src/touchservice/touchDeviceFactory.java
@@ -1,5 +1,9 @@
 package touchservice;
 
+import android.graphics.Bitmap;
+import net.atec.analyzer.Analizer;
+import net.atec.sender.DeviceEvent;
+import touchservice.devices.AtecDevice;
 import touchservice.devices.Gen_nexus_one;
 import touchservice.devices.SonyXperiaTXLT29i;
 import touchservice.devices.htc_new_one_m7;
@@ -7,7 +11,7 @@ import touchservice.devices.htc_one_x;
 
 public class touchDeviceFactory {
 	public static String[] getDeviceList() {
-		return new String[] { "htc_new_one_m7", "htc_one_x", "Gen_nexus_one", "SonyXperiaTXLT29i" };
+		return new String[] { "htc_new_one_m7", "htc_one_x", "Gen_nexus_one", "SonyXperiaTXLT29i","Others" };
 	}
 
 	public static AbstractTouchService getNewTouchService(String deviceName) {
@@ -25,6 +29,15 @@ public class touchDeviceFactory {
 		else if (deviceName.equals("SonyXperiaTXLT29i")) {
 			ret = new SonyXperiaTXLT29i();
 			ret.setUp(120, 60, 604);
+		}
+		else if (deviceName.equals("Others")) {
+			Analizer an = new Analizer();
+			DeviceEvent de = an.getDeviceEvent();
+			int width = Integer.parseInt(de.getScreenXMax());
+			int oneball = width/6;
+			int heigh = Integer.parseInt(de.getScreenYMax());
+			ret = new AtecDevice(de);
+			ret.setUp(oneball, oneball/2, (int)((heigh*0.45)+(oneball/2)));
 		}
 		return ret;
 	}
