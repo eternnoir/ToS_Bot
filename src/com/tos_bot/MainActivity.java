ef457dcaedd21a975e2cd5c3703837d20f7efb53
Stephen Chen <genius091612@gmail.com>
2013-09-24 01:01:57 +0800

    add exception strategy

--- src/com/tos_bot/MainActivity.java
+++ src/com/tos_bot/MainActivity.java
@@ -38,22 +38,28 @@ public class MainActivity extends Activity {
 	private Boolean _isStrategyLayoutExist = false;
 	private final LinkedHashMap<Integer, String> IdStringMap = new LinkedHashMap<Integer, String>() {
 		{
-			put(R.id.Vary_color_Single, "Vary_color_Single");
-			put(R.id.Vary_color_Multi, "Vary_color_Multi");
-			put(R.id.Water_Single, "Water_Single");
-			put(R.id.Water_Multi, "Water_Multi");
-			put(R.id.Fire_Single, "Fire_Single");
-			put(R.id.Fire_Multi, "Fire_Multi");
-			put(R.id.Wood_Single, "Wood_Single");
-			put(R.id.Wood_Multi, "Wood_Multi");
-			put(R.id.Light_Single, "Light_Single");
-			put(R.id.Light_Multi, "Light_Multi");
-			put(R.id.Dark_Single, "Dark_Single");
-			put(R.id.Dark_Multi, "Dark_Multi");
-			put(R.id.Recover_Single, "Recover_Single");
-			put(R.id.Recover_Multi, "Recover_Multi");
-			put(R.id.Low_HP_Single, "Low_HP_Single");
-			put(R.id.Low_HP_Multi, "Low_HP_Multi");
+			put(R.id.Vary_color_Single,     "Vary_color_Single");
+			put(R.id.Vary_color_Multi,      "Vary_color_Multi");
+			put(R.id.Water_Single,          "Water_Single");
+			put(R.id.Water_Multi,           "Water_Multi");
+			put(R.id.Fire_Single,           "Fire_Single");
+			put(R.id.Fire_Multi,            "Fire_Multi");
+			put(R.id.Wood_Single,           "Wood_Single");
+			put(R.id.Wood_Multi,            "Wood_Multi");
+			put(R.id.Light_Single,          "Light_Single");
+			put(R.id.Light_Multi,           "Light_Multi");
+			put(R.id.Dark_Single,           "Dark_Single");
+			put(R.id.Dark_Multi,            "Dark_Multi");
+			put(R.id.Recover_Single,        "Recover_Single");
+			put(R.id.Recover_Multi,         "Recover_Multi");
+            put(R.id.Water_Except,          "Water_Except");
+            put(R.id.Fire_Except,           "Fire_Except");
+            put(R.id.Wood_Except,           "Wood_Except");
+            put(R.id.Light_Except,          "Light_Except");
+            put(R.id.Dark_Except,           "Dark_Except");
+            put(R.id.Heart_Except,          "Heart_Except");
+			put(R.id.Low_HP_Single,         "Low_HP_Single");
+			put(R.id.Low_HP_Multi,          "Low_HP_Multi");
 		}
 	};
 
