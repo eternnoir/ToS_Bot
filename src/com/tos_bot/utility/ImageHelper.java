8457e4bf3bfb2c7d498659472a466376d81d9bb5
FrankWang <eternnoir@gmail.com>
2014-02-06 11:49:08 +0800

    add class ImageHelper

---
+++ src/com/tos_bot/utility/ImageHelper.java
@@ -0,0 +1,55 @@
+package com.tos_bot.utility;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.Matrix;
+
+/**
+ * @author frankwang
+ *
+ */
+public  class ImageHelper {
+
+	public ImageHelper(){
+	}
+	
+	static public void savePng(String Path, Bitmap bm) {
+		try {
+			File file = new File(Path + ".png");
+			file.createNewFile();
+			BufferedOutputStream out = new BufferedOutputStream(
+					new FileOutputStream(file));
+			bm.compress(CompressFormat.PNG, 100, out);
+
+			out.flush();
+			out.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	static public Bitmap resize(Bitmap bm, int newWidth, int newHeight){
+	    int width = bm.getWidth();
+	    int height = bm.getHeight();
+	    float scaleWidth = ((float) newWidth) / width;
+	    float scaleHeight = ((float) newHeight) / height;
+	    // CREATE A MATRIX FOR THE MANIPULATION
+	    Matrix matrix = new Matrix();
+	    // RESIZE THE BIT MAP
+	    matrix.postScale(scaleWidth, scaleHeight);
+
+	    // "RECREATE" THE NEW BITMAP
+	    Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
+	    return resizedBitmap;
+	}
+}
