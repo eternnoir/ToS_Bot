f172a0d1c0f4839a5171df51260a40ca7e7f7262
FrankWang <eternnoir@gmail.com>
2014-02-07 12:57:26 +0800

    add phash alog

--- src/com/tos_bot/utility/ImageHelper.java
+++ src/com/tos_bot/utility/ImageHelper.java
@@ -8,7 +8,11 @@ import java.io.IOException;
 
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Matrix;
+import android.graphics.Paint;
 
 /**
  * @author frankwang
@@ -18,6 +22,7 @@ public  class ImageHelper {
 
 	public ImageHelper(){
 	}
+
 	
 	static public void savePng(String Path, Bitmap bm) {
 		try {
@@ -51,5 +56,99 @@ public  class ImageHelper {
 	    // "RECREATE" THE NEW BITMAP
 	    Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
 	    return resizedBitmap;
+	}
+	
+	public static int[] BitmaptoGrayscale(Bitmap bmpOriginal)
+	{        
+		int color[] = new int[6]; // RGBDLH
+		int picw = bmpOriginal.getWidth();
+		int pich = bmpOriginal.getHeight();
+		int[] pix = new int[picw * pich];
+		int[] ret = new int[picw * pich];
+		bmpOriginal.getPixels(pix, 0, picw, 0, 0, picw, pich);
+		int r, g, b;
+		for (int i = 0; i < pix.length; i++) {
+			int _red = ((pix[i]) >> 16) & 0xFF;
+			int _green = ((pix[i]) >> 8) & 0xFF;
+			int _blue = ((pix[i])) & 0xFF;
+			ret[i]=(int) (0.3 * _red + 0.59 * _green + 0.11 * _blue);
+
+		}
+	    return pix;
+	}
+	
+	public static int average(int[] pixels) {
+		float m = 0;
+		for (int i = 0; i < pixels.length; ++i) {
+			m += pixels[i];
+		}
+		m = m / pixels.length;
+		return (int) m;
+	}
+	
+	public static int rgbToGray(int pixels) {
+		// int _alpha = (pixels >> 24) & 0xFF;
+		int _red = (pixels >> 16) & 0xFF;
+		int _green = (pixels >> 8) & 0xFF;
+		int _blue = (pixels) & 0xFF;
+		return (int) (0.3 * _red + 0.59 * _green + 0.11 * _blue);
+	}
+	
+	public static char binaryToHex(int binary) {
+		char ch = ' ';
+		switch (binary)
+		{
+		case 0:
+			ch = '0';
+			break;
+		case 1:
+			ch = '1';
+			break;
+		case 2:
+			ch = '2';
+			break;
+		case 3:
+			ch = '3';
+			break;
+		case 4:
+			ch = '4';
+			break;
+		case 5:
+			ch = '5';
+			break;
+		case 6:
+			ch = '6';
+			break;
+		case 7:
+			ch = '7';
+			break;
+		case 8:
+			ch = '8';
+			break;
+		case 9:
+			ch = '9';
+			break;
+		case 10:
+			ch = 'a';
+			break;
+		case 11:
+			ch = 'b';
+			break;
+		case 12:
+			ch = 'c';
+			break;
+		case 13:
+			ch = 'd';
+			break;
+		case 14:
+			ch = 'e';
+			break;
+		case 15:
+			ch = 'f';
+			break;
+		default:
+			ch = ' ';
+		}
+		return ch;
 	}
 }
---
+++ src/com/tos_bot/utility/SimilarImageSearch.java
@@ -0,0 +1,48 @@
+package com.tos_bot.utility;
+
+import android.graphics.Bitmap;
+
+public class SimilarImageSearch {
+	public static String produceFingerPrint(Bitmap bm) {
+		// TODO 
+		int width = 8;
+		int height = 8;
+		Bitmap sImg = ImageHelper.resize(bm, width, height);
+		
+		int[] pixels = ImageHelper.BitmaptoGrayscale(sImg);
+		sImg = null;
+		
+		int avgPixel = ImageHelper.average(pixels);
+		
+		int[] comps = new int[width * height];
+		for (int i = 0; i < comps.length; i++) {
+			if (pixels[i] >= avgPixel) {
+				comps[i] = 1;
+			} else {
+				comps[i] = 0;
+			}
+		}
+		
+		StringBuffer hashCode = new StringBuffer();
+		for (int i = 0; i < comps.length; i+= 4) {
+			int result = comps[i] * (int) Math.pow(2, 3) + comps[i + 1] * (int) Math.pow(2, 2) + comps[i + 2] * (int) Math.pow(2, 1) + comps[i + 2];
+			hashCode.append(ImageHelper.binaryToHex(result));
+		}
+		
+		return hashCode.toString();
+		
+	}
+	
+	public static int hammingDistance(String sourceHashCode, String hashCode) {
+		int difference = 0;
+		int len = sourceHashCode.length();
+		
+		for (int i = 0; i < len; i++) {
+			if (sourceHashCode.charAt(i) != hashCode.charAt(i)) {
+				difference ++;
+			} 
+		}
+		
+		return difference;
+	}
+}
